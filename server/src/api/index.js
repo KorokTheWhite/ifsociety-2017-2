425354c1a6eec8d435e685f0886537dbb26f62ce
Leonardo Alves <leunardosevla@gmail.com>
2017-09-29 16:51:37 -0300

    feat: added endpoint route to organization in index.js

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const example = require('./example');
+const organization = require('./organization');
 
 router.use('/example', example);
+router.use('/organization', organization);
 
 module.exports = router;
