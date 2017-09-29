244a060d12e21fb683050528f8bde74853cc8e96
Diogo Korok <korokdiogo@gmail.com>
2017-09-29 19:48:35 -0300

    feat(header): create header sketch

---
+++ app/src/components/header.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="">
+        <img src="">
+      </a>
+      <!-- add notification icon here -->
+      <button class="button navbar-burguer" v-bind:class="{'is-active' : burguerIsActive}" v-on:click="burguerClick()">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        burguerIsActive: false
+      }
+    },
+    methods() {
+      burguerClick() {
+        this.burguerIsActive = !this.burguerIsActive;
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scope>
+
+</style>
