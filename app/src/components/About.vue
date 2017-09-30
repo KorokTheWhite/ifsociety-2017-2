b08918b0920c9046916658da270280c89bd5b4b9
Diogo Korok <korokdiogo@gmail.com>
2017-09-30 10:30:20 -0300

    feat: create homepage

---
+++ app/src/components/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="about">
+    <navbar></navbar>
+    <div class="container">
+      <h1 class="title">{{ appName }}</h1>
+      <p>
+      Somos a {{ appName }} e nosso objetivo é facilitar o acesso de pessoas que se sentem
+      movidas a doar alimentos a fim de ajudar os desfavorecidos à ONGs que trabalham
+      com essa ideia mas que possuem escassez de recursos fincanceiros.
+      </p>
+    </div>
+    <footnote></footnote>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar';
+import Footnote from '@/components/Footnote';
+
+export default {
+  components: {
+    Navbar,
+    Footnote,
+  },
+  data() {
+    return {
+      appName: 'Nome Provisório',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 20px;
+}
+#footnote {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
