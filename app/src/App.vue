a826bd540116b9b8b3f4fb0ab05239f5a2a4c62d
Calvin Gerling <calvingerling@outlook.com>
2017-09-29 21:29:45 -0300

    feat: add the bulma customization

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,5 +9,18 @@ export default {
 };
 </script>
 <style lang="scss">
+@import "~bulma/bulma";
+@import "~bulma/sass/utilities/initial-variables";
 
+$palegreen: #26ff9b;
+$paleblue: #72d0eb;
+$green: #34f85b;
+$paleorange: #f26d4c;
+$orange: #cf1c44;
+
+$primary: $palegreen;
+$info: $paleblue;
+$success: $green;
+$warning: $paleorange;
+$danger: $orange;
 </style>
