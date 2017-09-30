ab1b6ad19811c7dad6318245a8858037dd796aaf
Leonardo Alves <leunardosevla@gmail.com>
2017-09-30 10:58:04 -0300

    feat: added call to api in auth service

--- app/src/services/auth.js
+++ app/src/services/auth.js
@@ -1,5 +1,8 @@
 import firebase from 'firebase';
 
+import * as Person from '@/api/person';
+import * as Organization from '@/api/organization';
+
 export function userLogged() {
   return firebase.auth().currentUser;
 }
@@ -12,10 +15,10 @@ export function login(email, password) {
   return firebase.auth().signInWithEmailAndPassword(email, password);
 }
 
-export function registerVolunteer() {
-
+export function registerVolunteer(name, email, uuid, cpf, address) {
+  Person.create(name, email, uuid, cpf, address);
 }
 
-export function registerOrganization() {
-
+export function registerOrganization(name, email, uuid, cnpj, cnas, address) {
+  Organization.create(name, email, uuid, cnpj, cnas, address);
 }
