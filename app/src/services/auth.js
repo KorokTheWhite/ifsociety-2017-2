51b14004e6cd62082adca4c496fef88f836916d3
Calvin Gerling <calvingerling@outlook.com>
2017-09-30 12:01:07 -0300

    feat(auth): implement the person register behaviour

--- app/src/services/auth.js
+++ app/src/services/auth.js
@@ -1,7 +1,11 @@
 import firebase from 'firebase';
 
-import * as Person from '@/api/person';
-import * as Organization from '@/api/organization';
+import * as person from '@/api/person';
+import * as organization from '@/api/organization';
+
+function createFirebaseUser(email, password) {
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
+}
 
 export function userLogged() {
   return firebase.auth().currentUser;
@@ -15,10 +19,16 @@ export function login(email, password) {
   return firebase.auth().signInWithEmailAndPassword(email, password);
 }
 
-export function registerVolunteer(name, email, uuid, cpf, address) {
-  Person.create(name, email, uuid, cpf, address);
+export function registerVolunteer(name, email, password, cpf, address) {
+  return new Promise((resolve, reject) => {
+    createFirebaseUser(email, password).then((user) => {
+      person.create(name, email, user.uid, cpf, address).then(() => {
+        resolve(user.uuid);
+      }).catch(reject);
+    }).catch(reject);
+  });
 }
 
 export function registerOrganization(name, email, uuid, cnpj, cnas, address) {
-  Organization.create(name, email, uuid, cnpj, cnas, address);
+  organization.create(name, email, uuid, cnpj, cnas, address);
 }
