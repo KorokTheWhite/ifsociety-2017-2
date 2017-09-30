87611d8fb547c2b22b71e61542a7920d2563da3a
Calvin Gerling <calvingerling@outlook.com>
2017-09-29 23:36:02 -0300

    feat(Login): add the base of login

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Login from '../components/Login';
 
 Vue.use(Router);
 
 export default new Router({
+  base: '/',
+  mode: 'history',
   routes: [
     {
+      path: '/access/login',
+      component: Login,
     },
   ],
 });
