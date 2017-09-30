c65711532c1cb2cc454ada306f2dcf0784f7bf06
Diogo Korok <korokdiogo@gmail.com>
2017-09-30 05:30:47 -0300

    refactor: make component to render donations with different status

--- app/src/components/OngFeed.vue
+++ app/src/components/OngFeed.vue
@@ -1,35 +1,15 @@
 <template>
   <div id="ong-feed">
-    <navbar></navbar>
-    <div class="card" v-for="donation in donations">
-      <header class="card-header">
-        <p class="card-header-title">
-          {{ donation.address }}
-        </p>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          <ul>
-            <li v-for="food in donation.foods">
-              {{ food }}
-            </li>
-          </ul>
-          <span>Horário para retirada: {{ donation.expiry.getDate() }}/{{ donation.expiry.getMonth() }}/{{ donation.expiry.getFullYear() }}</span>
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a class="card-footer-item">Estou interessado</a>
-      </footer>
-    </div>
+    <donation-list donations="donations"></donation-list>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar';
+import DonationList from '@/components/DonationList';
 
 export default {
   components: {
-    Navbar,
+    DonationList,
   },
   data() {
     return {
